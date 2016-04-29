7e1b373d6d79af853ef893c36c4c47ce5d759137
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-04-30 09:49:15 +1000

    using factorygirl for meme model tests

--- spec/factories/memes.rb
+++ spec/factories/memes.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :meme do
-    url "MyString"
+    sequence(:url) {|n| "http://images.memes.com/meme/#{2000 + n}.jpg"}
+    upvotes 0
+    downvotes 0
   end
 end
--- spec/models/meme_spec.rb
+++ spec/models/meme_spec.rb
@@ -2,18 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Meme, :type => :model do
   before :each do
-    @meme = Meme.new(url: "http://static.tumblr.com/ponuofb/E2Fm3kwyo/623573278216.jpg")
+    @meme = FactoryGirl.build(:meme)
   end
-
-  it "is valid with a url" do
+  
+  it "has a valid factory" do
     expect(@meme).to be_valid
   end
 
+  it "is invalid without a url" do
+    @meme.url = nil
+    expect(@meme).to_not be_valid
+  end
+
   it "is invalid with a duplicate url" do
-    dup_meme = Meme.create(url: "http://static.tumblr.com/ponuofb/E2Fm3kwyo/623573278216.jpg")
+    FactoryGirl.create(:meme, url: @meme.url)
     @meme.valid?
     expect(@meme.errors[:url]).to include("has already been taken")
-    dup_meme.destroy
   end
 
   it "is created with 0 upvotes" do
@@ -24,7 +28,7 @@ RSpec.describe Meme, :type => :model do
     expect(@meme.downvotes).to eq 0
   end
 
-  it "calculates and returns the net votes as an integer" do
+  it "calculates and returns net votes as an integer" do
     expect(@meme.votes).to eq 0
 
     @meme.upvotes = 5
